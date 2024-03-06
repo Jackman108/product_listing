20c7730293dd824b7dd573a935d30478e0306856
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2024-03-06 21:50:56 +0300

    Modified by Api

--- src/api/products.js
+++ src/api/products.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { generateAuthString } from '../utils/auth.js';
 import { removeDuplicateProductsId } from '../helpers/removeDuplicateProductsId.js';
 
-const API_URL = 'http://api.valantis.store:40000/'
+const API_URL = 'https://api.valantis.store:41000/'
 
 
 const handleRequestError = async (error, retryFunction) => {
